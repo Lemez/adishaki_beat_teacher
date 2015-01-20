16574a8396fba22a66a6194b8eb9724a7b33878f
Jonathan Walton <lemez9@gmail.com>
2015-01-20 14:36:30 +0530

    v1.0 all working with slightly strange behaviour in metronome if beat changed, and startstop bug still existing. Mizhavu playalong feature added, and credits layout improved

--- js/beats.js
+++ js/beats.js
@@ -1,10 +1,59 @@
 $(document).ready(function () {
 
-	var beatsData = {"three":{"one":[["RS","0","0","0","0","0","0","0"],["LR","0","0","0","0","0","0","0"],["RR","0","0","0","0","0","0","0"],["LS","0","0","0","0","0","0","0"],["RR","0","0","0","0","0","0","0"],["LR","0","0","0","0","0","0","0"]],"two":[["RS","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"]],"three":[["RS","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"]],"four":[["RS","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"]]},"four":{"one":[["RS","0","0","0","0","0","0","0"],["LR","0","0","0","0","0","0","0"],["RR","0","0","0","0","0","0","0"],["LR","0","0","0","0","0","0","0"]],"two":[["RS","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"]],"three":[["RS","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"]],"four":[["RS","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"]]},
+	$('#errors').text("Best Viewed on Chrome Desktop")
+				.css("visibility","visible")
+				.animate({
+				opacity: 1
+	},500)
+				.animate({
+				opacity: 0
+	},3000);
+
+	var beatsData = {
+
+	"three":{"one":[
+		["RS","0","0","0","0","0","0","0"],
+		["LR","0","0","0","0","0","0","0"],
+		["RR","0","0","0","0","0","0","0"],
+		["LS","0","0","0","0","0","0","0"],
+		["RR","0","0","0","0","0","0","0"],
+		["LR","0","0","0","0","0","0","0"]],
+		"two":[
+		["RS","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"]],
+		"three":[
+		["RS","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"]],
+		"four":[
+		["RS","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"]]},
+
+	"four":{"one":[
+		["RS","0","0","0","0","0","0","0"],
+		["LR","0","0","0","0","0","0","0"],
+		["RR","0","0","0","0","0","0","0"],
+		["LR","0","0","0","0","0","0","0"]],
+		"two":[
+		["RS","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"]],
+		"three":[
+		["RS","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"]],
+		"four":[
+		["RS","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"]]},
+
 	"seven":{
-
 		"one":[
-
 		["RR","0","0","0","0","0","0","0"],
 		["LR","0","0","0","0","0","0","0"],
 		["RS","0","0","0","0","0","0","0"],
@@ -19,33 +68,85 @@ $(document).ready(function () {
 		["RR","0","0","0","0","0","0","0"],
 		["LS","0","0","0","0","0","0","0"],
 		["RR","0","0","0","0","0","0","0"],
-		["LS","0","0","0","0","0","0","0"]
-		],
+		["LS","0","0","0","0","0","0","0"]],
+		"two":[
+		["RR","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"],
+		["RS","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"],
+		["RS","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"],
+		["RS","0","0","0","LA","0","0","0"]],
+		"three":[
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RS","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RS","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RS","0","LA","0","RA","0","LA","0"]],
+		"four":[
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RS","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RS","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RS","LA","RA","LA","RA","LA","RA","LA"]]},
 
+	"eight":{"one":[
+		["RR","0","0","0","0","0","0","0"],
+		["LR","0","0","0","0","0","0","0"],
+		["RR","0","0","0","0","0","0","0"],
+		["LS","0","0","0","0","0","0","0"],
+		["RR","0","0","0","0","0","0","0"],
+		["LS","0","0","0","0","0","0","0"],
+		["RR","0","0","0","0","0","0","0"],
+		["LS","0","0","0","0","0","0","0"]],
+		"two":[
+		["RR","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"],
+		["RS","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"],
+		["RS","0","0","0","LA","0","0","0"],
+		["RR","0","0","0","LA","0","0","0"],
+		["RS","0","0","0","LA","0","0","0"]],
+		"three":[
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RS","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RS","0","LA","0","RA","0","LA","0"],
+		["RR","0","LA","0","RA","0","LA","0"],
+		["RS","0","LA","0","RA","0","LA","0"]],
+		"four":[
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RS","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RS","LA","RA","LA","RA","LA","RA","LA"],
+		["RR","LA","RA","LA","RA","LA","RA","LA"],
+		["RS","LA","RA","LA","RA","LA","RA","LA"]]},
 
+	"threehalf":{"one":[
+		["RS","0","0","0","LR","0","0","0"],
+		["RS","0","0","0","LR","0","0","0"],
+		["RS","0","0","0","LR","0","0","0"],
+		["RR","0","0","0","LS","0","0","0"],
+		["RR","0","0","0","LS","0","0","0"],
+		["RR","0","0","0","LS","0","0","0"],
+		["RR","0","0","0","LR","0","0","0"]
+		]},
 
-	"two":[["RR","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"],["RS","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"],["RS","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"],["RS","0","0","0","LA","0","0","0"]],"three":[["RR","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"],["RS","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"],["RS","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"],["RS","0","LA","0","RA","0","LA","0"]],"four":[["RR","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"],["RS","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"],["RS","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"],["RS","LA","RA","LA","RA","LA","RA","LA"]]},
-
-	"eight":{"one":[["RR","0","0","0","0","0","0","0"],["LR","0","0","0","0","0","0","0"],["RR","0","0","0","0","0","0","0"],["LS","0","0","0","0","0","0","0"],["RR","0","0","0","0","0","0","0"],["LS","0","0","0","0","0","0","0"],["RR","0","0","0","0","0","0","0"],["LS","0","0","0","0","0","0","0"]],"two":[["RR","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"],["RS","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"],["RS","0","0","0","LA","0","0","0"],["RR","0","0","0","LA","0","0","0"],["RS","0","0","0","LA","0","0","0"]],"three":[["RR","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"],["RS","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"],["RS","0","LA","0","RA","0","LA","0"],["RR","0","LA","0","RA","0","LA","0"],["RS","0","LA","0","RA","0","LA","0"]],"four":[["RR","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"],["RS","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"],["RS","LA","RA","LA","RA","LA","RA","LA"],["RR","LA","RA","LA","RA","LA","RA","LA"],["RS","LA","RA","LA","RA","LA","RA","LA"]]},"threehalf":{"one":[
-
-
-	["RS","0","0","0","LR","0","0","0"],
-	["RS","0","0","0","LR","0","0","0"],
-	["RS","0","0","0","LR","0","0","0"],
-	["RR","0","0","0","LS","0","0","0"],
-	["RR","0","0","0","LS","0","0","0"],
-	["RR","0","0","0","LS","0","0","0"],
-	["RR","0","0","0","LR","0","0","0"]
-
-
-	]},"five":{"one":[
-
-	["RS","0","0","0","RR","0","LR","0"],
-	["RR","0","LR","0","RR","0","0","0"],
-	["RR","0","LS","0","0","0","RR","0"],
-	["LR","0","RR","0","LR","0","0","0"],
-	["RS","0","0","0","0","0","0","0"]
-	
+	"five":{"one":[
+		["RS","0","0","0","RR","0","LR","0"],
+		["RR","0","LR","0","RR","0","0","0"],
+		["RR","0","LS","0","0","0","RR","0"],
+		["LR","0","RR","0","LR","0","0","0"],
+		["RS","0","0","0","0","0","0","0"]
 	]}}
 
 
@@ -86,7 +187,7 @@ $(document).ready(function () {
 	console.log(isMobile);
 	console.log(platform);
 	resize;
-	var gameIsOn;
+	var state;
 	setup();
 
 
@@ -94,7 +195,7 @@ $(document).ready(function () {
 
 	function setup(){
 
-	gameIsOn = false;
+	state = "play";
 	var errorState = false;
 
 	var ERRORS = {
@@ -105,7 +206,14 @@ $(document).ready(function () {
 	function raiseError(thing) {
 
 		$('#errors').text(ERRORS[thing]);
-		$('#errors').css('visibility','visible');
+		$('#errors').css('visibility','visible')
+	 		.animate({
+	 			opacity: 1
+	 		},750)
+	 		.animate({
+	 			opacity: 0
+	 		},1000);
+
 		errorState = true;
 	}
 
@@ -156,14 +264,27 @@ $(document).ready(function () {
 
 	});
 
+	$('#credits').on('click',function(){$('#credit_section').show(250);});
+	$('#close_credits').on('click', function(){$('#credit_section').hide(250);});
+
+
+	
 
 // pressing stop/go
 	$('#play').on('click', function(){
 
 		checkforerrors();
+		var state = $('#play').attr('class');
+		var game;
 		
-		if (errorState==false) {
-			togglePlayState(true);
+		if (errorState==false&&state=="play") {
+			start();
+			$('#errors').css('visibility','hidden');
+			
+			}
+
+		else if (errorState==false&&state=="stop") {
+			stopplaying(game);
 			$('#errors').css('visibility','hidden');
 			
 			}
@@ -174,13 +295,13 @@ $(document).ready(function () {
 	}
 
 // start the game
-	function startgame() {
-		gameIsOn = true;
+	function begingame() {
+
+		var bpm = document.getElementById("tempo_value").value;
 
 		myBeat =  $(".active").attr("value");
 		myLevel =  $(".active").attr("name");
 
-		var bpm = document.getElementById("tempo_value").value;
       	var beatMs = 60000.0 / bpm;		
 		// console.log(myBeat,myLevel,beatMs);
 
@@ -195,33 +316,66 @@ $(document).ready(function () {
 		}
 
 		playData(myBeat,myLevel,beatMs,bars);
+
+
 		function playData(beat,level,beatMs,bars) {
-			
-			animateDrums(data[0], beat, level, 0);	
-			playBeat(0,beat,level,beatMs);
+
+			var mizhavustate= $('#mizhavu').is(':checked');
+			var metrostate= $('#metro').is(':checked');
+
+			animateDrums(data[0], beat, level, 0,mizhavustate);
+
+			playBeat(0,beat,level,beatMs,metrostate);
 
 			var game = setInterval(function(){
 				
 				if (i < bars) {
+
+					var d = new Date();
+					var n = d.getTime();
+					
 					var currentBar = data[i];
+
+					console.log(currentBar);
+
+					var mizhavustate = false;
+
+					mizhavustate= $('#mizhavu').is(':checked');
+					var metrostate= $('#metro').is(':checked');
 				
-					animateDrums(currentBar, beat, level, i);
-					playBeat(i,beat,level,beatMs);
+					animateDrums(currentBar, beat, level, i,mizhavustate);
+					playBeat(i,beat,level,beatMs,metrostate);
+
+					var e = new Date();
+					var o = e.getTime();
+
+					console.log(n-o);
+
+
+
 				}
 				i++;
 			
 				if (i==bars){i=0;} 	// infinite loop
 
 			// set break functions
-				$('#play').on('click', function(){i=bars+1;	var end = true; });
-				$('nav ul li').on('click', function(){i=bars+1;
-										startgame(); return false
-					// setTimeout(togglePlayState(game),beatMs)
+			// $('li a')[0].click();
+				$('#play').on('click', function(){ 
+					i=bars+1;
+					 });
+				$('nav ul li').on('click', function(){
+					i=bars+1;
+					stopplaying(game);
 				});
 
 				$('#temposelect').on('change',function(){
-					i=bars+1;
-					startgame();
+					
+					clearInterval(game);
+					stopplaying(game);
+					begingame();
+
+
+
 
 				});
 
@@ -240,7 +394,7 @@ $(document).ready(function () {
 			$('#count').text(count.toString());
 		}
 
-		function animateDrums(beat_array,beat,level,index) {
+		function animateDrums(beat_array,beat,level,index,mizhavustate) {
 
 			var j = 1;
 			var side = "L";
@@ -279,16 +433,22 @@ $(document).ready(function () {
 					if (pos=="L")		{var side = $('#text_left');}
 					else {var side = $('#text_right');}
 
+					// play mizhavu
+					console.log(mizhavustate);
+					if (mizhavustate==true&&beatExists==true) {playMizhavu(stressed)};
+
 					// show text(L/R) if there is a beat at all
 					if (beatExists==true){
 
 						cleanDrumsText();
 
 						if(beat=="threehalf"){console.log(index,j,beat)}
+
 						updateCounter(index,beat,level);
 
 						// select the text for the correct drum
 						side.text(pos);
+						
 
 						// make it big or small
 						if (stressed==true //&& level=="one" || stressed==true && level=="two"
@@ -314,15 +474,19 @@ $(document).ready(function () {
 	// end of startgame()
 		
 
-	function playBeat(b,beat,level,beatMs){
+	function playBeat(b,beat,level,beatMs,metro){
 
 		setTimeout(function () {
 
-			if (b=="0")
+			if (b=="0"&&metro==true)
 				{document.getElementById("strong").play();}
-			else if (b=="3"&&beat=="three"&&level=="one")
+
+			else if (b=="3"&&beat=="three"&&level=="one"&&metro==true)
 				{document.getElementById("strong").play();}
-			else {document.getElementById("weak").play();}
+
+			else if (metro==true){document.getElementById("weak").play();}
+
+
 
 			if (level=="three"||level=="four"){shakeDrum();}
 
@@ -342,27 +506,26 @@ $(document).ready(function () {
 
 		}
 
-	function togglePlayState(game){
-		clearInterval(game);
-		var state = $('nav span#play').attr('class');
-		console.log(state);
-		startStopGame();
-
-		if (state=='play')		{$('#play').removeClass('play').addClass('stop');}
-		else if (state=='stop')	{$('#play').removeClass('stop').addClass('play');}
-	}
-
-	function startStopGame(){
-
-		if (!gameIsOn) {
-			gameIsOn = true;
-			startgame();
-			
-		} else {
-			gameIsOn = false;
-			setup();
+		function playMizhavu(s) {
+			if (s==true){document.getElementById("stressbeat").play();} else {document.getElementById("regularbeat").play();}
 		}
+
+
+
+
+	function start(){
+		$('#play').removeClass('play').addClass('stop');
+		var bpm = document.getElementById("tempo_value").value;
+		begingame();
 	}
+
+	function stopplaying(game){
+		clearInterval(game);
+		$('nav #controls p a').trigger("click");
+		$('#play').removeClass('stop').addClass('play'); 
+	}
+
+	
 
 
 
